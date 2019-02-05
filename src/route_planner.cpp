4275d89d3a8f133bd3393f096bb30de0bf5f432f
Stephen Welch <swelch53@gmail.com>
2019-02-05 14:17:43 -0800

    Minor changes.

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,7 +1,5 @@
 #include "route_planner.h"
-#include <cmath>
 #include <algorithm>
-#include <set>
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
     // Convert inputs to percentage:
